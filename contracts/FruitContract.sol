9d46c7acd3819a93e1167601f1c69cff6307260c
GianluBlock <gianluca360hc@gmail.com>
2023-10-10 22:35:25 +0200

    tax system  only selll on dex, tax 15 fix on buy

--- contracts/FruitContract.sol
+++ contracts/FruitContract.sol
@@ -21,6 +21,8 @@ contract ERC20Token {
     uint256 public taxPercentageC = 5; // Fixed 5% tax
     uint256 public liquidityTaxPercentage = 3; // 3% liquidity tax
 
+    address public constant DEX_ADDRESS = 0x10ED43C718714eb63d5aA57B78B54704E256024E; // PancakeSwapV2 Router Address
+
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
@@ -69,43 +71,34 @@ contract ERC20Token {
         uint256 initialTaxPercentage;
         if (msg.sender == addressA) {
             initialTaxPercentage = taxPercentageA;
-        } else {
-            initialTaxPercentage = taxPercentageB;
-        }
-
-        // Check if 3 days have passed since the contract deployment
-        if (block.timestamp >= (block.timestamp + 3 days)) {
-            // Decrease tax percentage from 27% to 10%
-            if (initialTaxPercentage == 27) {
-                if (msg.sender == addressA) {
+            
+            // Check if 3 days have passed since the contract deployment
+            if (block.timestamp >= (block.timestamp + 3 days)) {
+                // Decrease tax percentage from 27% to 10%
+                if (initialTaxPercentage == 27) {
                     taxPercentageA = 10;
                 } else {
-                    taxPercentageB = 10;
+                    revert("ERC20: Invalid initial tax percentage for addressA");
                 }
+            } else {
+                revert("ERC20: Cannot decrease tax before 3 days");
             }
+        } else if (msg.sender == addressB) {
+            initialTaxPercentage = taxPercentageB;
+            
             // Check if 20 days have passed since the contract deployment
-            else if (block.timestamp >= (block.timestamp + 20 days)) {
-                // Decrease tax percentage from 10% to 4%, and from 4% to 3%
+            if (block.timestamp >= (block.timestamp + 20 days)) {
+                // Decrease tax percentage from 10% to 4%, and from 7% to 3%
                 if (initialTaxPercentage == 10) {
-                    if (msg.sender == addressA) {
-                        taxPercentageA = 4;
-                    } else {
-                        taxPercentageB = 4;
-                    }
-                } else if (initialTaxPercentage == 4) {
-                    if (msg.sender == addressA) {
-                        taxPercentageA = 3;
-                    } else {
-                        taxPercentageB = 3;
-                    }
+                    taxPercentageB = 4;
+                } else if (initialTaxPercentage == 7) {
+                    taxPercentageB = 3;
                 } else {
-                    revert("ERC20: Tax percentage can only be decreased from 27% to 10%, then from 10% to 4%, and finally from 4% to 3%");
+                    revert("ERC20: Invalid initial tax percentage for addressB");
                 }
             } else {
                 revert("ERC20: Cannot decrease tax before 20 days");
             }
-        } else {
-            revert("ERC20: Cannot decrease tax before 3 days");
         }
     }
 
@@ -121,18 +114,16 @@ contract ERC20Token {
 
         uint256 taxAmount = 0;
 
-        // Check if both sender and receiver are not in the tax system (P2P transaction)
-        if (_from != addressA && _from != addressB && _from != addressC &&
-            _to != addressA && _to != addressB && _to != addressC) {
-            // P2P transaction, no tax applied
-        } else {
-            // Tax calculation for addressA, addressB, or addressC
-            if (msg.sender == addressA) {
-                taxAmount = (_value * taxPercentageA) / 100;
-            } else if (msg.sender == addressB) {
-                taxAmount = (_value * taxPercentageB) / 100;
-            } else if (msg.sender == addressC) {
-                taxAmount = (_value * taxPercentageC) / 100;
+        // Check if it's a sell transaction (from non-tax address to DEX)
+        bool isSellTransaction = (_from != addressA && _from != addressB && _from != addressC && _to == DEX_ADDRESS);
+
+        // Apply tax logic only for sell transactions
+        if (isSellTransaction) {
+            // Tax calculation for addressA and addressB
+            if (_from == addressA) {
+                taxAmount = (_value * 4) / 100; // 4% tax for addressA
+            } else if (_from == addressB) {
+                taxAmount = (_value * 3) / 100; // 3% tax for addressB
             }
         }
 
@@ -144,11 +135,10 @@ contract ERC20Token {
         balanceOf[_from] -= _value;
         balanceOf[_to] += transferAmount;
 
-        // Apply tax only if it's not a P2P transaction
+        // Apply tax only if it's a sell transaction
         if (taxAmount > 0) {
-            balanceOf[addressA] += taxAmount / 3; // Distribute taxAmount equally among addressA, addressB, and addressC
-            balanceOf[addressB] += taxAmount / 3;
-            balanceOf[addressC] += taxAmount / 3;
+            balanceOf[addressA] += taxAmount / 2; // Distribute taxAmount among addressA and addressB
+            balanceOf[addressB] += taxAmount / 2;
         }
 
         totalSupply -= burnAmount;
@@ -157,9 +147,8 @@ contract ERC20Token {
 
         // Emit tax and burn events only if they are applicable
         if (taxAmount > 0) {
-            emit Transfer(_from, addressA, taxAmount / 3);
-            emit Transfer(_from, addressB, taxAmount / 3);
-            emit Transfer(_from, addressC, taxAmount / 3);
+            emit Transfer(_from, addressA, taxAmount / 2);
+            emit Transfer(_from, addressB, taxAmount / 2);
         }
         emit Transfer(_from, address(0), burnAmount); // Burn event
     }
